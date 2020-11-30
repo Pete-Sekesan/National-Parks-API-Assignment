1df233d2eac22e633660936100ef31b29b038655
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-11-30 13:59:43 -0500

    finished commits

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
 //create api and search url header
-const apiKey = 'UmctMZrZlKl6DiKxCR1NXCzwzdkiOfAVA1IHr8WV'; 
+const apiKey = 'UmctMZrZlKl6DiKxCR1NXCzwzdkiOfAVA1IHr8WV';
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 //format the query results for use in URL
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
-      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
+//create a get parks function
+function getParks(location, maxResults = 10) {
+    const params = {
+        stateCode: state,
+        api_key: apiKey,
+        maxResults,
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+    //check for correct url
+    console.log(url);
 
-    //create a get parks function
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => {
+            $('#error-message').text(`Oops! Something went wrong: ${error.message}`);
+        });
+
+}
+
+function displayResults(responseJson) {
+    //clear previous results
+    $('#results').empty();
+
+    for (let i = 0; i < responseJson.data.length; i++) {
+        $('#results-list').append(
+            <li> <h2> $ {responseJson.data[i].fullName} </h2> 
+            <p> ${responseJson.data[i].description} </p> 
+            <p> $ {responseJson.data[i].url} </p> 
+            </li >
+            )
+        }
+
+    watchForm() {
+        $('form').submit(event => {
+            event.preventDefault();
+            const location = $('#search-state').val();
+            const maxResults = $('#search-max-results').val();
+            getParks(location, maxResults)
+        })
+    }
+}
+$(watchForm)
