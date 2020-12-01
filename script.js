1884e5d62d06239fa5849dd3c18389a0685b5108
Pete Sekesn <72456128+The-Spruce-Goose@users.noreply.github.com>
2020-12-01 09:40:22 -0500

    fixed multi-state code

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const apiKey = 'UmctMZrZlKl6DiKxCR1NXCzwzdkiOfAVA1IHr8WV';
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 //format the query results for use in URL
-function formatQueryParams(params) {
+/*function formatQueryParams(params) {
     const queryItems = Object.keys(params)
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
-}
+}*/
 //create a get parks function
 function getParks(state, limit = 10) {
     const params = {
@@ -16,8 +16,8 @@ function getParks(state, limit = 10) {
         api_key: apiKey,
         limit: limit
     };
-    const queryString = formatQueryParams(params)
-    const url = searchURL + '?' + queryString;
+    //const queryString = formatQueryParams(params)
+    const url = `${searchURL}?api_key=${apiKey}&stateCode=${state}&limit=${limit}`;
     //check for correct url
     console.log(url)
 
